7ea8aa0ba94b2e76784896a42ca8bb67607abf7d
Riyaz Rizvi <785syedshoaib@gmail.com>
2024-04-14 00:50:02 +0530

    complete Setup For Project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements()->list[str]:
+    requirements_list =list [str]=[]
+    return requirements_list
+
+
+
+
+setup(
+name="Sensor",
+version="0.0.1",
+author="Riyaz",
+author_email="785syedshoaib@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements(), #["pymongo"]
+
+
+
+
+
+
+)
